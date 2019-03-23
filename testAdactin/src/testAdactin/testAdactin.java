af673dd5a9a53fb39cc2f405bae41f4795b6357b
edwin lobo <edwinmartinlobo@gmail.com>
2019-03-23 12:13:08 -0400

    test

--- testAdactin/src/testAdactin/testAdactin.java
+++ testAdactin/src/testAdactin/testAdactin.java
@@ -4,7 +4,7 @@ package testAdactin;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class testAdactin {
+class testAdactin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
